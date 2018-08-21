04d8131f996d7781948e1cd16570a0407f214a15
Kavitha Tiptur Madhu <kmadhu@anl.gov>
2018-08-21 16:49:32 -0500

    cleanup

--- workstealing-benchmark.c
+++ workstealing-benchmark.c
@@ -9,10 +9,9 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-#define N 20
-#define QUEUE_SIZE 65536
-#define THRESHOLD 2
-#define STEAL_ATTEMPTS_THRESHOLD 5000
+#define N 10
+#define QUEUE_SIZE 2048
+#define THRESHOLD 8
 
 typedef struct task {
 	int arg;
@@ -22,7 +21,7 @@ typedef struct task {
 typedef struct task_return {
 	int arg;
 	enum return_type {
-		TASK, INTEGER
+		TASK, IMMEDIATE
 	} type;
 	union {
 		struct task_value {
@@ -51,7 +50,7 @@ void fib_function(void * thread_return_value) {
 				fib_value = new_fib_value;
 			}
 		}
-		return_value.type = INTEGER;
+		return_value.type = IMMEDIATE;
 		return_value.value.value = fib_value;
 	} else {
 		task* children = (task*) calloc(2, sizeof(task));
@@ -73,7 +72,7 @@ void fib_function(void * thread_return_value) {
 int main(int argc, char ** argv) {
 	int rank, size;
 	task * task_queue = (task *) calloc(QUEUE_SIZE, sizeof(task));
-	/* Front pointer at 0, back pointer at 1, spin lock at 2*/
+	/* Front pointer at 1, back pointer at 2, spin lock at 0*/
 	int task_ptr[3];
 	MPI_Win task_win, task_ptr_win;
 	unsigned int seed;
@@ -83,7 +82,6 @@ int main(int argc, char ** argv) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
 	int compare_value = -1;
 	int lock_value = rank;
 	int prev_value;
@@ -105,13 +103,15 @@ int main(int argc, char ** argv) {
 		task_ptr[2] = -1;
 	}
 
-	MPI_Win_create(&task_queue[0], QUEUE_SIZE * sizeof(struct task), sizeof(struct task), MPI_INFO_NULL,
+	double start_time = MPI_Wtime();
+
+	MPI_Info info = MPI_INFO_NULL;
+	MPI_Win_create(&task_queue[0], QUEUE_SIZE * sizeof(struct task), sizeof(struct task), info,
 	MPI_COMM_WORLD, &task_win);
 
 	MPI_Win_create(&task_ptr[0], 3, sizeof(int), MPI_INFO_NULL,
 	MPI_COMM_WORLD, &task_ptr_win);
 
-//	MPI_Win_lock_all(MPI_MODE_NOCHECK, task_ptr_win);
 	MPI_Win_lock_all(MPI_MODE_NOCHECK, task_win);
 
 	int remoteflag[3] = { 0, 0, -1 };
@@ -119,10 +119,11 @@ int main(int argc, char ** argv) {
 	current.arg = -1;
 
 	while (1) {
-		MPI_Barrier(MPI_COMM_WORLD);
 		int global_num_attempts = num_attempts;
 		MPI_Request request;
-		MPI_Allreduce(&num_attempts, &global_num_attempts, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+		MPI_Status status;
+		MPI_Iallreduce(&num_attempts, &global_num_attempts, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD, &request);
+		MPI_Wait(&request,&status);
 		if (global_num_attempts > 0) {
 			break;
 		}
@@ -133,39 +134,37 @@ int main(int argc, char ** argv) {
 		int i = rank;
 		while(count++<size) {
 			int remoteflag[3] = { 0, 0, -1 };
-
-			MPI_Win_lock_all(MPI_MODE_NOCHECK, task_ptr_win);
+			MPI_Win_lock(MPI_LOCK_SHARED, i, MPI_MODE_NOCHECK, task_ptr_win);
 			MPI_Compare_and_swap(&lock_value, &compare_value, &prev_value, MPI_INT, i, 2, task_ptr_win);
 			MPI_Get(&remoteflag[0], 3, MPI_INT, i, 0, 3, MPI_INT, task_ptr_win);
 			MPI_Win_flush(i, task_ptr_win);
-			MPI_Win_unlock_all(task_ptr_win);
+			MPI_Win_unlock(i, task_ptr_win);
 			MPI_Win_fence(MPI_MODE_NOCHECK, task_ptr_win);
 
 			if(remoteflag[2] == rank &&  current.task_id == -1 && remoteflag[0] != remoteflag[1] && remoteflag[1] != -1 && remoteflag[0] < QUEUE_SIZE) {
-				MPI_Win_lock_all(MPI_MODE_NOCHECK, task_ptr_win);
+				MPI_Win_lock(MPI_LOCK_SHARED, i, MPI_MODE_NOCHECK, task_ptr_win);
 				MPI_Get(&current, sizeof(struct task), MPI_BYTE, i, remoteflag[0], sizeof(struct task), MPI_BYTE, task_win);
 				remoteflag[0] = (remoteflag[0] + 1) % QUEUE_SIZE;
-				MPI_Accumulate(&remoteflag[0], 1,
+				MPI_Put(&remoteflag[0], 1,
 				MPI_INT, i, 0, 1,
-				MPI_INT, MPI_REPLACE, task_ptr_win);
+				MPI_INT, task_ptr_win);
 				MPI_Win_flush(rank, task_ptr_win);
-				MPI_Win_flush(rank, task_win);
-				MPI_Win_unlock_all(task_ptr_win);
+				MPI_Win_unlock(i, task_ptr_win);
 			}
 			MPI_Win_fence(MPI_MODE_NOCHECK, task_ptr_win);
 
-			MPI_Win_lock_all(MPI_MODE_NOCHECK, task_ptr_win);
+			MPI_Win_lock(MPI_LOCK_SHARED, i, MPI_MODE_NOCHECK, task_ptr_win);
 			MPI_Compare_and_swap(&compare_value, &lock_value, &prev_value, MPI_INT, i, 2, task_ptr_win);
-			MPI_Win_flush_all(task_ptr_win);
-			MPI_Win_unlock_all(task_ptr_win);
+			MPI_Win_flush(i, task_ptr_win);
+			MPI_Win_unlock(i, task_ptr_win);
 			i = (i+1)%size;
 		}
 		MPI_Win_fence(MPI_MODE_NOCHECK, task_ptr_win);
 
-		MPI_Win_lock_all(MPI_MODE_NOCHECK, task_ptr_win);
+		MPI_Win_lock(MPI_LOCK_SHARED, rank, MPI_MODE_NOCHECK, task_ptr_win);
 		MPI_Compare_and_swap(&lock_value, &compare_value, &prev_value, MPI_INT, rank, 2, task_ptr_win);
 		MPI_Win_flush(rank, task_ptr_win);
-		MPI_Win_unlock_all(task_ptr_win);
+		MPI_Win_unlock(rank, task_ptr_win);
 		MPI_Win_fence(MPI_MODE_NOCHECK, task_ptr_win);
 
 		if (current.task_id >= 0) {
@@ -174,8 +173,7 @@ int main(int argc, char ** argv) {
 			return_value->arg = current.arg;
 			pthread_create(&thread, NULL, (void*) fib_function, return_value);
 			pthread_join(thread, NULL);
-			if (return_value->type == INTEGER) {
-//				printf("fib value computed as %ld\n", return_value->value.value);
+			if (return_value->type == IMMEDIATE) {
 				sum += return_value->value.value;
 			} else if(task_ptr[2] == rank){
 				/* Queue the tasks in the local task queue */
@@ -186,26 +184,25 @@ int main(int argc, char ** argv) {
 						MPI_Abort(MPI_COMM_WORLD,  MPI_ERR_OTHER);
 					}
 				}
-
 			}
 			num_attempts = 0;
 		} else {
 			num_attempts++;
 		}
 
-		MPI_Win_lock_all(MPI_MODE_NOCHECK, task_ptr_win);
+		MPI_Win_lock(MPI_LOCK_SHARED, rank, MPI_MODE_NOCHECK, task_ptr_win);
 		MPI_Compare_and_swap(&compare_value, &lock_value, &prev_value, MPI_INT, rank, 2, task_ptr_win);
 		MPI_Win_flush(rank, task_ptr_win);
-		MPI_Win_unlock_all(task_ptr_win);
+		MPI_Win_unlock(rank, task_ptr_win);
 		MPI_Win_fence(MPI_MODE_NOCHECK, task_ptr_win);
-		MPI_Barrier(MPI_COMM_WORLD);
 	}
 	MPI_Win_unlock_all(task_win);
-	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Allreduce(MPI_IN_PLACE, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 	printf("global sum:%ld\n", sum);
 	MPI_Win_free(&task_win);
 	MPI_Win_free(&task_ptr_win);
+	double end_time = MPI_Wtime();
+	printf("Time: %lf\n", ((end_time-start_time)*10e6));
 	free(task_queue);
 	return 0;
 }
